a3e83068184f9902c96719e248bea00f1d1838b4
Ruslan Dautov <dautov2@gmail.com>
2026-06-12 12:50:28 +0200

    Add BigExport cask

---
+++ Casks/bigexport.rb
@@ -0,0 +1,15 @@
+cask "bigexport" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/dautovri/gotoapplemaps-bigexport/releases/latest/download/BigExport-latest.dmg"
+  name "BigExport"
+  desc "Import thousands of saved places from Google Maps, CSV, or KML into Apple Maps"
+  homepage "https://gotoapplemaps.app"
+
+  app "BigExport.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.GoToAppleMaps.BigExport.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# dautovri/homebrew-tap
+
+Homebrew tap for [GoToAppleMaps](https://gotoapplemaps.app) tools.
+
+## Install
+
+```sh
+brew install --cask dautovri/tap/bigexport
+```
